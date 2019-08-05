b204b7ff4458ee6de847462d29588048bb34740d
gjq <gjq@gjqdeiMac.local>
2019-08-06 00:17:31 +0800

    增加normalize.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "styles/iconfont/iconfont.css";
-import "styles/common.scss";
-import "styles/reset.css";
+import "styles/normalize.scss";
+import "styles/default.scss"
 import "./utils/prototype"
 export default class App extends Component {
   componentDidMount() {}
